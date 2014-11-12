bd630d7364e7f6aeac9965c78598a02f5fa9d81b
Steve Rudolfi <srudolfi@bu.edu>
2014-11-12 11:00:33 -0500

    Adding GruntFile

--- demo/Gruntfile.js
+++ demo/Gruntfile.js
@@ -5,33 +5,34 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 
 		// 2. All functions go here.
-		watch: {
-			grunt: {
-				files: [ 'Gruntfile.js'],
-				options: {
-					reload: true
-				}
-			},
-			scripts: {
-				files: [
-				'js-dev/*.js',
-				'js/libs/**/*.js'
-				],
-				tasks: ['concat', 'uglify'],
-				options: {
-					spawn: false,
-				},
-			},
-			styles: {
-				files: [
-				'css-dev/*.scss'
-				],
-				tasks: ['sass'],
-				options: {
-					spawn: false,
-				}
-			}
-		},
+		
+		// watch: {
+// 			grunt: {
+// 				files: [ 'Gruntfile.js'],
+// 				options: {
+// 					reload: true
+// 				}
+// 			},
+// 			scripts: {
+// 				files: [
+// 				'js-dev/*.js',
+// 				'js/libs/**/*.js'
+// 				],
+// 				tasks: ['concat', 'uglify'],
+// 				options: {
+// 					spawn: false,
+// 				},
+// 			},
+// 			styles: {
+// 				files: [
+// 				'css-dev/*.scss'
+// 				],
+// 				tasks: ['sass'],
+// 				options: {
+// 					spawn: false,
+// 				}
+// 			}
+// 		},
 		concat: {
 			scripts: {
 				src: [
@@ -40,44 +41,46 @@ module.exports = function(grunt) {
 				dest: 'js/script.js',
 			}
 		},
-		uglify: {
-			scripts: {
-				expand: true,
-				cwd: 'js',
-				src: ['*.js', '!*.min.js'],
-				dest: 'js',
-				ext: '.min.js'
-			},
-			vendor: {
-				expand: true,
-				cwd: 'js/vendor',
-				src: ['**/*.js', '!**/*.min.js'],
-				dest: 'js/vendor',
-				ext: '.min.js'
-			}
-		},
-		sass: {
-			styles: {
-				options: {
-					style: 'compressed',
-					sourcemap: true,
-					loadPath: '/'
-				},
-				files: {
-					'css/style.css': 'css-dev/base.scss',
-				}
-			}
-		}
+// 		uglify: {
+// 			scripts: {
+// 				expand: true,
+// 				cwd: 'js',
+// 				src: ['*.js', '!*.min.js'],
+// 				dest: 'js',
+// 				ext: '.min.js'
+// 			},
+// 			vendor: {
+// 				expand: true,
+// 				cwd: 'js/vendor',
+// 				src: ['**/*.js', '!**/*.min.js'],
+// 				dest: 'js/vendor',
+// 				ext: '.min.js'
+// 			}
+// 		},
+// 		sass: {
+// 			styles: {
+// 				options: {
+// 					style: 'compressed',
+// 					sourcemap: true,
+// 					loadPath: '/'
+// 				},
+// 				files: {
+// 					'css/style.css': 'css-dev/base.scss',
+// 				}
+// 			}
+// 		}
 	});
+	
 
 	// 3. Where we tell Grunt we plan to use this plug-in.
-	grunt.loadNpmTasks('grunt-contrib-watch');
+	//grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-sass');
+	//grunt.loadNpmTasks('grunt-contrib-uglify');
+	//grunt.loadNpmTasks('grunt-contrib-sass');
 
 	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-	grunt.registerTask('build', ['sass', 'concat', 'uglify']);
-	grunt.registerTask('default', ['watch']);
+	//grunt.registerTask('build', ['sass', 'concat', 'uglify']);
+	//grunt.registerTask('default', ['watch']);
+	grunt.registerTask('default', ['concat']);
 
 };
