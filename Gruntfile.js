30318c0d6a714ebc087095cfd31e86d3fb5f385d
Mike Burns <mgburns@bu.edu>
2014-11-13 14:26:38 -0500

    Snapshot of demo site when completed.
    Updates Gruntfile.js to end-of-presentation state.
    Adds generates JS / CSS files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,6 @@ module.exports = function(grunt) {
 				dest: 'js/script.js'
 			}
 		},
-		/*
 		uglify: {
  			scripts: {
 				src: ['js/script.js'],
@@ -58,18 +57,17 @@ module.exports = function(grunt) {
 				}
 			}
 		}
-	*/
 	});
 
 
 	// 3. Where we tell Grunt we plan to use this plug-in.
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	//grunt.loadNpmTasks('grunt-contrib-uglify');
-	//grunt.loadNpmTasks('grunt-contrib-watch');
-	//grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 
 	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-	grunt.registerTask('build', ['concat']);
-	//grunt.registerTask('default', ['concat']);
-	//grunt.registerTask('default', ['watch']);
+	grunt.registerTask('build', ['concat', 'uglify', 'sass']);
+	// grunt.registerTask('default', ['build']);
+	grunt.registerTask('default', ['watch']);
 };
