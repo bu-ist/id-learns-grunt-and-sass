157f1786c7e199c0dae6838f6ebaa813734dfe92
Steve Rudolfi <srudolfi@bu.edu>
2014-11-13 12:01:18 -0500

    Updating package.json, cleaning up gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,71 +7,69 @@ module.exports = function(grunt) {
 		// 2. All functions go here.
 		concat: {
 			scripts: {
-				src: [
-					'js-dev/*.js'
-				],
-				dest: 'js/script.js',
+				src: ['js-dev/*.js'],
+				dest: 'js/script.js'
 			}
 		},
-//		uglify: {
-// 			scripts: {
-// 				expand: true,
-// 				cwd: 'js',
-// 				src: ['*.js', '!*.min.js'],
-// 				dest: 'js',
-// 				ext: '.min.js'
-// 			}
-// 		},
-// 		watch: {
-// 			grunt: {
-// 				files: [ 'Gruntfile.js'],
-// 				options: {
-// 					reload: true
-// 				}
-// 			},
-// 			scripts: {
-// 				files: [
-// 				'js-dev/*.js',
-// 				],
-// 				tasks: ['concat', 'uglify'],
-// 				options: {
-// 					spawn: false,
-// 				},
-// 			},
-// 			styles: {
-// 				files: [
-// 				'css-dev/*.scss'
-// 				],
-// 				tasks: ['sass'],
-// 				options: {
-// 					spawn: false,
-// 				}
-// 			}
-// 		},
-// 		sass: {
-// 			styles: {
-// 				options: {
-// 					style: 'compressed',
-// 					sourcemap: true,
-// 					loadPath: '/'
-// 				},
-// 				files: {
-// 					'css/style.css': 'css-dev/base.scss',
-// 				}
-// 			}
-// 		}
+		/*
+		uglify: {
+ 			scripts: {
+				src: ['js/script.js'],
+ 				dest: 'js/script.min.js'
+ 			}
+ 		},
+		watch: {
+			scripts: {
+				files: [
+				'js-dev/*.js',
+				],
+				tasks: ['concat', 'uglify'],
+				options: {
+					spawn: false,
+				},
+			},
+			styles: {
+				files: [
+				'css-dev/*.scss'
+				],
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				}
+			}
+		},
+		sass: {
+			devl: {
+				options: {
+					style: 'expanded',
+					sourcemap: 'auto'
+				},
+				files: {
+					'style.css': 'css-dev/base.scss',
+				}
+			},
+			prod: {
+				options: {
+					style: 'compressed',
+					sourcemap: 'auto'
+				},
+				files: {
+					'style.min.css': 'css-dev/base.scss',
+				}
+			}
+		}
+	*/
 	});
 	
 
 	// 3. Where we tell Grunt we plan to use this plug-in.
-	//grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	//grunt.loadNpmTasks('grunt-contrib-uglify');
+	//grunt.loadNpmTasks('grunt-contrib-watch');
 	//grunt.loadNpmTasks('grunt-contrib-sass');
 
 	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-	//grunt.registerTask('build', ['sass', 'concat', 'uglify']);
+	grunt.registerTask('build', ['concat']);
+	//grunt.registerTask('default', ['concat']);
 	//grunt.registerTask('default', ['watch']);
-	grunt.registerTask('default', ['concat']);
-
 };
